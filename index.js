4e20644e2839f314e67eee6e6fc8609ca36be2cd
vwede oteghele <louisshaw75@gmail.com>
2021-04-01 02:15:09 -0700

    css styling

--- index.js
+++ index.js
@@ -1,89 +1,69 @@
- baseUrl = 'https://api.lyrics.ovh/v1/'
- contentDiv =document.querySelector('.lyrical')
+ //api to display the lyrics of a specific song
+ lyricsBaseUrl = 'https://api.lyrics.ovh/v1/'
+ lyricsDiv =document.querySelector('.lyrical')
 
- const getDataFromApi = async () => {
+ const getLyricsDataFromApi = async () => {
      try{
-         const response = await fetch(`${baseUrl}Coldplay/Adventure of a Lifetime`)
-         console.log(response);
-        const JsonFormat = await response.json();
-         console.log(JsonFormat)
-         const lyrics = await JsonFormat.lyrics
-         return lyrics
+         const lyricsBaseUrlResponse = await fetch(`${lyricsBaseUrl}Coldplay/Adventure of a Lifetime`)
+            const JsonLyricsData = await lyricsBaseUrlResponse.json();
+            const lyrics = await JsonLyricsData.lyrics
+                return lyrics
      }
-
      catch(error){
          console.log("API unavailable")
     }
  }
 
-// //getDataFromApi();
-
  window.addEventListener('load', async() => {
-     let returnedPromise = await getDataFromApi()
-     contentDiv.innerHTML = returnedPromise;
+     let lyricsData = await getLyricsDataFromApi()
+        lyricsDiv.innerHTML = lyricsData;
  })
 
-// // window.addEventListener('load', async () =>{
-// //     const workData = await getDataFromApi()
-// //     const firstWork = workData[0]
-// // })
 
-// // console.log(firstWork)
-
-
-const baseUrl1 = 'https://www.boredapi.com/api/'
-const contentDiv1 = document.querySelector('.content')
+//api to display activity when a user click the 'i'm bored' button
+const boredBaseUrl = 'https://www.boredapi.com/api/'
+const boredDiv = document.querySelector('.content')
 const button = document.querySelector('.boredbtn')
 
-const getDataFromApi1 = async () => {
+const getBoredDataFromApi = async () => {
     try {
-        const response1 = await fetch(`${baseUrl1}activity/`)
-         console.log(response1)
-            const jsonFormat = response1.json()
-             console.log(jsonFormat)
-             return jsonFormat
+        const boredApiResponse = await fetch(`${boredBaseUrl}activity/`)
+            const jsonBoredFormat = boredApiResponse.json()
+                 return jsonBoredFormat
     }
     catch (error){
             console.log('api is currently unavailable for your consumption')
     }
 }
 
-
-
 button.addEventListener('click', async () => {
-    const returnedPromise1 = await getDataFromApi1()
-    console.log(returnedPromise1)
-    contentDiv1.innerHTML = returnedPromise1.activity
+    const boredData = await getBoredDataFromApi()
+    boredDiv.innerHTML = boredData.activity
 })
 
-const baseUrl2 = 'https://cat-fact.herokuapp.com'
-const contentDiv2 = document.querySelector('.contentclass1')
+//api to display cat facts with load event
+const catFactsBaseUrl = 'https://cat-fact.herokuapp.com'
+const catFactsDiv = document.querySelector('.contentclass')
 
-const getDataFromApi2 = async () => {
+const getcatFactsDataFromApi = async () => {
     try{
-        const response2 = await fetch(`${baseUrl2}/facts`)
-        console.log(response2)
-        const Json = await response2.json()
-        console.log(Json)
-        console.log(Json[0].text)
-        return Json
+        const catFactsApiResponse = await fetch(`${catFactsBaseUrl}/facts`)
+        const catFactsJson = await catFactsApiResponse.json()
+            return catFactsJson
     }
     catch(error){
         console.log('api is down or unavailable')
     }
 }
 
-
 window.addEventListener('load', async () => {
-    const returnedPromise2 = await getDataFromApi2()
-    let HTMLfacts = returnedPromise2.map((fact, i = 1) => {
+    const CatFactsData = await getcatFactsDataFromApi()
+    let displayHtmlData = CatFactsData.map((fact, i = 1) => {
         return `
         <h3>Fact ${i}: ${fact.text}</h3>
         <hr>
         `
     })
-
-    let formattedHTMLfacts = HTMLfacts.join(' ')
-    console.log(HTMLfacts)
-    contentDiv2.innerHTML = formattedHTMLfacts;
+    let formattedCatFacts = displayHtmlData.join(' ')
+    catFactsDiv.innerHTML = formattedCatFacts;
 }) 
